d1924f90dd182e9142f3fd5795530586715496ab
Emin Nefic <emin.nefic@gmail.com>
2025-03-11 20:31:37 +0100

    tese

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = withBundleAnalyzer(nextConfig);
